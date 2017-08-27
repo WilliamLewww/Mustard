fb10e6dc20533d32529c49175d60d5dab9e26d95
William Lew <WilliamMilesLew@gmail.com>
2017-08-26 20:26:23 -0700

    Initial commit

---
+++ main.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <ctime>
+
+static const int SCREENWIDTH = 1600, SCREENHEIGHT = 900;
+static const double pi = 3.14159265359;
+
+static SDL_Window* displayWindow;
