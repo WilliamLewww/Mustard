4c02e2b25fdda96bca5c9df7343f91ba5363ebb5
William Lew <WilliamMilesLew@gmail.com>
2017-08-28 23:51:03 -0700

    added SAT concept folder

---
+++ Separate Axis Theorem/main.cpp
@@ -0,0 +1,52 @@
+#include <SDL2\SDL.h>
+#include <SDL2\SDL_opengl.h>
+
+void update(int elapsedTime);
+void render(SDL_Window* window, SDL_GLContext context);
+
+static const int SCREENWIDTH = 1600, SCREENHEIGHT = 900;
+static const double pi = 3.14159265359;
+
+SDL_Event event;
+SDL_GLContext context;
+SDL_Window* displayWindow;
+
+bool isRunning = true;
+int frameStart, frameEnd, deltaTime = 0;
+int main(int argc, char *argv[]) {
+	displayWindow = SDL_CreateWindow("Separate Axis Theorem", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENWIDTH, SCREENHEIGHT, SDL_WINDOW_OPENGL);
+	context = SDL_GL_CreateContext(displayWindow);
+	glOrtho(-SCREENWIDTH / 2, SCREENWIDTH / 2, SCREENHEIGHT / 2, -SCREENHEIGHT / 2, 0, 1);
+
+	while (isRunning) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT)
+				isRunning = false;
+		}
+
+		if (deltaTime < 1 / 60) {
+			frameStart = SDL_GetTicks();
+			SDL_Delay(1);
+			frameEnd = SDL_GetTicks();
+			deltaTime = frameEnd - frameStart;
+		}
+		frameStart = SDL_GetTicks();
+		update(deltaTime);
+		render(displayWindow, context);
+		frameEnd = SDL_GetTicks();
+		deltaTime = frameEnd - frameStart; 
+	}
+
+	return 0;
+}
+
+void update(int elapsedTime) {
+}
+
+void render(SDL_Window* window, SDL_GLContext context) {
+	SDL_GL_MakeCurrent(window, context);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+
+	SDL_GL_SwapWindow(window);
+}
