509d3a46770469505966c0eae103f8986b51a53b
William Lew <WilliamMilesLew@gmail.com>
2018-02-09 19:09:31 -0800

    re-created main using a cleaner method

--- main.cpp
+++ main.cpp
@@ -1,117 +1,94 @@
 #include "main.h"
-#include <fstream>
 
-void initialize();
-void update(int elapsedTime);
-void render(SDL_Window* window, SDL_GLContext context);
+Engine engine;
 
-void grabConfig();
-
-int generationStyle = -1;
-int SCREENWIDTH = 1920, SCREENHEIGHT = 1080;
-
-Joiner joiner;
-
-SDL_Event event;
-SDL_GLContext context;
-
-bool isRunning = true; 
-int frameStart, frameEnd, deltaTime = 0;
 int main(int argc, char *argv[]) {
-	grabConfig();
-
-	displayWindow = SDL_CreateWindow("Hotdog", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENWIDTH, SCREENHEIGHT, SDL_WINDOW_OPENGL);
-	context = SDL_GL_CreateContext(displayWindow);
-	glOrtho(-SCREENWIDTH / 2, SCREENWIDTH / 2, SCREENHEIGHT / 2, -SCREENHEIGHT / 2, 0, 1);
-
-	initialize();
-
-	while (isRunning) {
-		removeInitialPress();
-		leftButtonPress = false;
-		middleMousePress = false;
-
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT)
-				isRunning = false;
-
-			getKeys(event);
-			getButtons(event);
-		}
-
-		if (deltaTime < 1 / 60) {
-			frameStart = SDL_GetTicks();
-			SDL_Delay(1);
-			frameEnd = SDL_GetTicks();
-			deltaTime = frameEnd - frameStart;
-		}
-		frameStart = SDL_GetTicks();
-		update(deltaTime);
-		render(displayWindow, context);
-		frameEnd = SDL_GetTicks();
-		deltaTime = frameEnd - frameStart; 
-	}
-	
-	SDL_Quit();
+	engine.initialize();
+	engine.start();
+	engine.quit();
 
 	return 0;
 }
 
-void initialize() {
-	SDL_Init(SDL_INIT_GAMECONTROLLER);
-	getController();
+void Engine::initialize() {
+	setScreenWidth(1000);
+	setScreenHeight(600);
 
-	glEnable(GL_BLEND); 
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	displayWindow = SDL_CreateWindow("Thane", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_OPENGL);
+	context = SDL_GL_CreateContext(displayWindow);
+	glOrtho(-screenWidth / 2, screenWidth / 2, screenHeight / 2, -screenHeight / 2, 0, 1);
 
-	srand(time(NULL));
-	joiner.initialize(generationStyle);
+	initializeExternalController();
 }
 
-void update(int elapsedTime) {
-	if (std::find(keyList.begin(), keyList.end(), SDLK_ESCAPE) != keyList.end()) {
+void Engine::initializeExternalController() {
+	SDL_Init(SDL_INIT_GAMECONTROLLER);
+	input.setupController();	
+}
+
+void Engine::start() {
+	while (isRunning) {
+		handleEvents();
+		delayEarlyFrames();
+		updateAndRender();
+	}
+}
+
+void Engine::quit() {
+	SDL_Quit();
+}
+
+void Engine::handleEvents() {
+	input.clearExpiredInput();
+
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
+			isRunning = false;
+		}
+
+		handleUserInput();
+	}
+}
+
+void Engine::handleUserInput() {
+	input.getKeyTrigger(event);
+	input.getMouseTrigger(event);
+}
+
+void Engine::delayEarlyFrames() {
+	if (deltaTime < 1 / 60) {
+		frameStart = SDL_GetTicks();
+		SDL_Delay(1);
+		frameEnd = SDL_GetTicks();
+		deltaTime = frameEnd - frameStart;
+	}
+}
+
+void Engine::updateAndRender() {
+	frameStart = SDL_GetTicks();
+	update();
+	render();
+	frameEnd = SDL_GetTicks();
+	deltaTime = frameEnd - frameStart; 
+}
+
+void Engine::handleEscapeKey() {
+	if (std::find(input.getKeyList().begin(), input.getKeyList().end(), SDLK_ESCAPE) != input.getKeyList().end()) {
 		isRunning = false;
 	}
-	
-	joiner.update(elapsedTime);
 }
 
-void render(SDL_Window* window, SDL_GLContext context) {
-	SDL_GL_MakeCurrent(window, context);
-	glClearColor(convertColor(28), convertColor(107), convertColor(160), 1);
+void Engine::update() {
+	handleEscapeKey();
+}
+
+void Engine::render() {
+	SDL_GL_MakeCurrent(displayWindow, context);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 
 	glPushMatrix();
-	joiner.draw();
 	glPopMatrix();
 
-	SDL_GL_SwapWindow(window);
-}
-
-int readStep = 0;
-void grabConfig() {
-	std::ifstream fin("config.txt");
-
-	int x;
-	while (fin >> x) {
-		switch (readStep) {
-			case 0:
-				if (x > 1920) { SCREENWIDTH = 1920; }
-				else { SCREENWIDTH = x; }
-				readStep += 1;
-				break;
-			case 1:
-				if (x > 1080) { SCREENHEIGHT = 1080; }
-				else { SCREENHEIGHT = x; }
-				readStep += 1;
-				break;
-			case 2:
-				generationStyle = x;
-				readStep += 1;
-				break;
-		}
-	}
-
-	fin.close();
+	SDL_GL_SwapWindow(displayWindow);
 }
--- main.h
+++ main.h
@@ -1,8 +1,37 @@
-#pragma once
-#include "src\input.h"
-#include "src\joiner.h"
 #include <SDL2\SDL.h>
 #include <SDL2\SDL_opengl.h>
-#include <ctime>
+#include "src\input.h"
 
-static SDL_Window* displayWindow;
+class Engine {
+private:
+	Input input;
+
+	SDL_Event event;
+	SDL_GLContext context;
+	SDL_Window* displayWindow;
+
+	int screenWidth, screenHeight;
+
+	bool isRunning = true; 
+	int frameStart, frameEnd, deltaTime = 0;
+
+public:
+	inline void setScreenWidth(int screenWidth) { this->screenWidth = screenWidth; }
+	inline void setScreenHeight(int screenHeight) { this->screenHeight = screenHeight; }
+	inline int getScreenWidth() { return screenWidth; }
+	inline int getScreenHeight() { return screenHeight; }
+
+	void initialize();
+	void initializeExternalController();
+	void start();
+	void quit();
+	void handleEvents();
+	void handleUserInput();
+	void handleEscapeKey();
+	void delayEarlyFrames();
+	void updateAndRender();
+	void update();
+	void render();
+};
+
+extern Engine engine;
